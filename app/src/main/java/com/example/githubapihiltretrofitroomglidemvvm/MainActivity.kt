1b64c710686384d35f5b85a90d565a2a1c760870
Bruno Braga <braga.bruno81@gmail.com>
2022-09-16 07:08:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/githubapihiltretrofitroomglidemvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.githubapihiltretrofitroomglidemvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
